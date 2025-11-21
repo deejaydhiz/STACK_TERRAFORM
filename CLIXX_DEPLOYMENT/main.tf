9d84774971678acafca6f586727a4ee85e38a095
deejaydhiz <djbanire@gmail.com>
2025-11-20 23:42:19 -0500

    CLIXX_DEPLOYMENT_TERRAFORM_v1.2: Create_Highly_Availabe_EFS

--- CLIXX_DEPLOYMENT/main.tf
+++ CLIXX_DEPLOYMENT/main.tf
@@ -11,3 +11,54 @@ resource "aws_db_instance" "clixx_db" {
     ignore_changes = [snapshot_identifier]
   }
 }
+
+
+# Create EFS for for clixx network file sharing 
+
+resource "aws_efs_file_system" "clixx_efs" {
+  creation_token    = "clixx-web"
+  encrypted         = true
+
+  tags = {
+    Name = "Clixx-EFS"
+  }
+}
+
+# Attach EFS to mount targets in each az for high availability
+
+resource "aws_efs_mount_target" "east1a" {
+  file_system_id  = aws_efs_file_system.clixx_efs.id
+  subnet_id       = "subnet-0a3d701d4ae56bd83"
+  security_groups = [aws_security_group.clixx_sg.id]
+}
+
+resource "aws_efs_mount_target" "east1b" {
+  file_system_id  = aws_efs_file_system.clixx_efs.id
+  subnet_id       = "subnet-096c55d1b79478358"
+  security_groups = [aws_security_group.clixx_sg.id]
+}
+
+resource "aws_efs_mount_target" "east1c" {
+  file_system_id  = aws_efs_file_system.clixx_efs.id
+  subnet_id       = "subnet-02b9ff9a66612dba1"
+  security_groups = [aws_security_group.clixx_sg.id]
+}
+
+resource "aws_efs_mount_target" "east1d" {
+  file_system_id  = aws_efs_file_system.clixx_efs.id
+  subnet_id       = "subnet-0cea1d9a41e99ed45"
+  security_groups = [aws_security_group.clixx_sg.id]
+}
+
+resource "aws_efs_mount_target" "east1e" {
+  file_system_id  = aws_efs_file_system.clixx_efs.id
+  subnet_id       = "subnet-00a91d6c6d1743e8f"
+  security_groups = [aws_security_group.clixx_sg.id]
+}
+
+resource "aws_efs_mount_target" "east1f" {
+  file_system_id  = aws_efs_file_system.clixx_efs.id
+  subnet_id       = "subnet-05393b0e3f17eeeae"
+  security_groups = [aws_security_group.clixx_sg.id]
+}
+
